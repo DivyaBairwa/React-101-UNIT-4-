79fad7cb7be466ee4f1cf894930d7755e2582022
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2022-05-16 23:44:22 +0530

    mobile

---
+++ Sprint-1/Day1/mobile/src/App.js
@@ -0,0 +1,12 @@
+import P1 from "./components/list";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <P1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Sprint-1/Day1/mobile/src/components/list.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const P1=()=>{
+    return(
+    <>
+        <div>
+            <h1>Mobile Operation System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>iPhone</li>
+                <li>Windows Phone</li>
+            </ul>
+            
+        </div>
+        <div>
+            <h1>Mobile Manufactures</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Microphone</li>
+                <li>Apple</li>
+            </ul>
+            
+        </div>
+   </>
+      
+    )
+}
+export  default P1;
