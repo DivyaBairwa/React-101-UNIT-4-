23e68f85632f8eaaccdf4bbfd52b0b64477cd725
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2022-05-27 00:58:17 +0530

    gtimer

---
+++ Sprint-2/Day3/gtimer/src/App.js
@@ -0,0 +1,12 @@
+import { GoogleTimer } from "./components/timer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleTimer />
+    </div>
+  );
+}
+
+export default App;
